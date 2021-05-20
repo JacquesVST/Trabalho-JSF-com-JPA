6e2a770ad39a57ec74faa75d371d8e92e50b9e49
Luan Mantuani <luangmantuani@gmail.com>
2021-05-20 00:56:48 -0300

    Refatoração do controller geral

--- src/main/java/fema/Controlador.java
+++ src/main/java/fema/Controlador.java
@@ -1,11 +1,8 @@
 package fema;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
@@ -14,24 +11,8 @@ public class Controlador implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private List<Banda> listaBanda = new ArrayList<Banda>();
-	
-	@Inject
-	private BandaDao bandaDao;
-
-	public void mostrarDados() {
-		listaBanda = bandaDao.getBandas();
-		for (Banda b : listaBanda) {
-			System.out.println(b.getNome());
-		}
+	public String irParaPagina(String pagina) {
+		return pagina;
 	}
 
-	public List<Banda> getListaBanda() {
-		return listaBanda;
-	}
-
-	public void setListaBanda(List<Banda> listaBanda) {
-		this.listaBanda = listaBanda;
-	}
-	
 }
